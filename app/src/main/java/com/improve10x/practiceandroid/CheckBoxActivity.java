f84364f479185b1eb603ae18d3a23d7ac62beed0
K Hareesh <hareesh.komma@improve10x.com>
2023-01-25 14:59:18 +0530

    created a xml design of CheckBoxActivity

--- app/src/main/java/com/improve10x/practiceandroid/CheckBoxActivity.java
+++ app/src/main/java/com/improve10x/practiceandroid/CheckBoxActivity.java
@@ -10,5 +10,6 @@ public class CheckBoxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_box);
+        getSupportActionBar().setTitle("CheckBox");
     }
 }
