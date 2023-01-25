af860d08a1f6e3c6781043d64f2b4e3b9740689c
K Hareesh <hareesh.komma@improve10x.com>
2023-01-25 17:36:31 +0530

    we implemented RadioButtonsActivity

--- app/src/main/java/com/improve10x/practiceandroid/RadioButtonsActivity.java
+++ app/src/main/java/com/improve10x/practiceandroid/RadioButtonsActivity.java
@@ -3,12 +3,34 @@ package com.improve10x.practiceandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.improve10x.practiceandroid.databinding.ActivityCheckBoxBinding;
+import com.improve10x.practiceandroid.databinding.ActivityRadioButtonsBinding;
 
 public class RadioButtonsActivity extends AppCompatActivity {
+
+    ActivityRadioButtonsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_radio_buttons);
+        binding = ActivityRadioButtonsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("Radio Buttons");
+        handleButton();
+    }
+
+    private void handleButton() {
+        binding.selectBtn.setOnClickListener(view -> {
+            if(binding.maleRadio.isChecked()) {
+                Toast.makeText(this, "Male", Toast.LENGTH_LONG).show();
+            }
+            if (binding.femaleRadio.isChecked()) {
+                Toast.makeText(this, "Female", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
