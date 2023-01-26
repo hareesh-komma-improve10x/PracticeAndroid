a6814b21f325a885aeabf5760c27147139cfd318
K Hareesh <hareesh.komma@improve10x.com>
2023-01-26 10:26:08 +0530

    we implemented the RatingBarActivity

--- app/src/main/java/com/improve10x/practiceandroid/RatingBarActivity.java
+++ app/src/main/java/com/improve10x/practiceandroid/RatingBarActivity.java
@@ -3,13 +3,31 @@ package com.improve10x.practiceandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.improve10x.practiceandroid.databinding.ActivityRatingBarBinding;
 
 public class RatingBarActivity extends AppCompatActivity {
+
+    ActivityRatingBarBinding binding;
+    RatingBar ratingBarRb;
+    Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rating_bar);
+        binding = ActivityRatingBarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().setTitle("RatingBar");
+        handleButton();
+    }
+
+    private void handleButton() {
+        binding.submitBtn.setOnClickListener(view -> {
+            String rating = String.valueOf(binding.ratingbarRb.getRating());
+            Toast.makeText(this, rating, Toast.LENGTH_SHORT).show();
+        });
     }
 }
