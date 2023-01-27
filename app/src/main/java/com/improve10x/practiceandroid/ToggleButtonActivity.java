a8bc1584751b2a6e009f8d393435d6af99718d1a
K Hareesh <hareesh.komma@improve10x.com>
2023-01-27 15:20:52 +0530

    I have applied UI Components and Business Logics to the ToggleButtonActivity

--- app/src/main/java/com/improve10x/practiceandroid/ToggleButtonActivity.java
+++ app/src/main/java/com/improve10x/practiceandroid/ToggleButtonActivity.java
@@ -24,10 +24,17 @@ public class ToggleButtonActivity extends AppCompatActivity {
 
     private void handleButton() {
         binding.submitBtn.setOnClickListener(view -> {
-            StringBuilder result = new StringBuilder();
-            result.append("button1Btn: ").append(binding.button1Btn.getText());
-            result.append("button2Btn: ").append(binding.button2Btn.getText());
-            Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT).show();
+            String button1 = binding.button1Btn.getText().toString();
+            String button2 = binding.button2Btn.getText().toString();
+            String result = getData(button1, button2);
+            Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
         });
+    }
+
+    private String getData(String button1, String button2) {
+        String status1 = "Button1 : " + button1 + "\n";
+        String status2 = "Button2 : " + button2;
+        String result = status1 += status2;
+        return result;
     }
 }
