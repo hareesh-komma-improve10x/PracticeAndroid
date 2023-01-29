df6f583e6e57266db0e55f6ba2689e4f7a783203
K Hareesh <hareesh.komma@improve10x.com>
2023-01-29 22:15:33 +0530

    I created the AlertDialogueBoxActivity

---
+++ app/src/main/java/com/improve10x/practiceandroid/AlertDialogueBoxActivity.java
@@ -0,0 +1,17 @@
+package com.improve10x.practiceandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlertDialogueBoxActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aleart_dialogue_box);
+        getSupportActionBar().setTitle("AlertDialog");
+    }
+}
