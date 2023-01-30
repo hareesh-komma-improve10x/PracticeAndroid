e3e4dffc0a73f3ace2cc3a9a60c35c155850f150
K Hareesh <hareesh.komma@improve10x.com>
2023-01-30 15:38:08 +0530

    we implemented the SearchViewActivity

--- app/src/main/java/com/improve10x/practiceandroid/SearchViewActivity.java
+++ app/src/main/java/com/improve10x/practiceandroid/SearchViewActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.improve10x.practiceandroid.databinding.ActivitySerachViewBinding;
 
@@ -21,5 +24,44 @@ public class SearchViewActivity extends AppCompatActivity {
         binding = ActivitySerachViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().setTitle("SearchView");
+        setData();
+        handleSearchView();
+    }
+
+    private void handleSearchView() {
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        binding.listLv.setAdapter(adapter);
+        binding.searchViewSv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (list.contains(query)) {
+                    adapter.getFilter().filter(query);
+                } else {
+                    Toast.makeText(SearchViewActivity.this, "No Match Found", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void setData() {
+        list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cat");
+        list.add("Dog");
+        list.add("Elephant");
+        list.add("Fan");
+        list.add("Gun");
+        list.add("Hareesh");
+        list.add("Icon");
+        list.add("Jug");
+        list.add("Knight");
+        list.add("Lion");
     }
 }
