d077af00aa5d017305085a8a0d169ffe2a79dcad
K Hareesh <hareesh.komma@improve10x.com>
2023-01-31 17:49:04 +0530

    we created a MultiAutoCompleteTextViewActivity design

---
+++ app/src/main/java/com/improve10x/practiceandroid/MultiAutoCompleteTextViewActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.practiceandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiAutoCompleteTextViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_auto_complete_text_view);
+    }
+}
