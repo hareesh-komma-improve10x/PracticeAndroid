a6eb6acb3fde7e18d9614c4ca4eea4b8a660592b
K Hareesh <hareesh.komma@improve10x.com>
2023-01-31 21:55:17 +0530

    we implemented MultiAutoCompleteTextViewActivity

--- app/src/main/java/com/improve10x/practiceandroid/MultiAutoCompleteTextViewActivity.java
+++ app/src/main/java/com/improve10x/practiceandroid/MultiAutoCompleteTextViewActivity.java
@@ -3,12 +3,25 @@ package com.improve10x.practiceandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
+
+import com.improve10x.practiceandroid.databinding.ActivityMultiAutoCompleteTextViewBinding;
 
 public class MultiAutoCompleteTextViewActivity extends AppCompatActivity {
+
+    private ActivityMultiAutoCompleteTextViewBinding binding;
+    private String[] androidVersionNames = {"Aestro", "Blender", "CupCake", "Donut", "Eclair", "Froyo", "Gingerbread", "HoneyComb", "IceCream Sandwich"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multi_auto_complete_text_view);
+        binding = ActivityMultiAutoCompleteTextViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        MultiAutoCompleteTextView simpleMultiAutoCompleteTextView = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView);
+        ArrayAdapter<String> versionNames = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, androidVersionNames);
+        simpleMultiAutoCompleteTextView.setAdapter(versionNames);
+        simpleMultiAutoCompleteTextView.setThreshold(1);
+        simpleMultiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
     }
 }
